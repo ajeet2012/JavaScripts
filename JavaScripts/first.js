43196aeba636f2fcc51dd8aa085fcdb6a2b8a589
ajeet2012 <ajeetmca2012@gmail.com>
2026-09-18 16:31:38 +0530

    Java script - basics

---
+++ JavaScripts/first.js
@@ -0,0 +1,69 @@
+
+
+fullName = "ajeet sharma";
+age = 39;
+price = 99.99
+console.log(fullName);
+console.log(age);
+console.log(price);
+x = null;
+y = undefined;
+console.log(x);
+console.log(y);
+
+isStudent = true
+
+console.log(isStudent)
+
+console.log("ajeet sharma. I am looking for job changes");
+console.log("ajeet sharma. I am looking for job changes");
+
+console.log("----  Data Types -----");
+
+let age2 = 39;
+let price2 = 34.50
+let isFollow = false
+
+let x2 = null;
+let bInt = BigInt("1212212121234");
+ 
+let student = {
+fullName : "ajeet sharma",
+age : 39,
+address: "nanda nagar, Indore, MP"
+};
+
+const student2 = {
+    fullName : "ravi sharma",
+    age : 33
+}
+
+student2.age = 34;
+console.log(student2.age);
+
+console.log("Amazon product example")
+
+const product = {
+    title : "Camera",
+    price : 200,
+    rating: 4,
+    offer: 20
+}
+
+let str = "abc"
+let str2 = str + 123
+
+console.log(str2) // Output will be abc123 
+
+console.log("Insta profile data");
+
+const profile = {
+
+    username : "@ajeet",
+    isFollows : true,
+    followers : 12112,
+    following : 454 
+}
+console.log(profile);
+console.log(typeof profile);
+console.log(typeof profile.username);
