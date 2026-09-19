349011c55d1ebc0975e9e9e3bd3cf5bea54a666c
ajeet2012 <ajeetmca2012@gmail.com>
2026-09-19 22:17:22 +0530

    JS - ForEach Problems

---
+++ JavaScripts/forEachProblem.js
@@ -0,0 +1,12 @@
+// for a given array of numbers, print the value of each value using the forEach loop
+
+console.log("for a given array of numbers, print the value of each value using the forEach loop");
+
+
+let numberArr = [12, 3, 44, 5, 89, 9];
+
+numberArr.forEach((val, index) => {
+
+    console.log(val * val);
+
+});
