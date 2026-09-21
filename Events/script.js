116d38799efac08f91ecd3970868e542d7b86b6f
ajeet2012 <ajeetmca2012@gmail.com>
2026-09-21 13:30:34 +0530

    JS - Add/Remove Event Listeners

--- Events/script.js
+++ Events/script.js
@@ -12,8 +12,30 @@ btn1.onclick = (evt) => {
 
 //Onmouseover event - 
 let div = document.querySelector("#divJS");
-console.dir(div)
 div.onmouseover = (event) => {
     console.log("Mouse over on new div");
     console.log(event.clientX, event.clientY); // 124 130
 };
+
+// Event Listners - We can add more than one listner for one element and all will be worked.
+
+let btn2 = document.querySelector("#btn2");
+// handler 1
+btn2.addEventListener("click", (evt) => {
+    console.log("Handle Event Listner");
+    console.log(evt.target); // <button id="btn2">Event Listner</button>
+});
+
+// handler 2
+btn2.addEventListener("click", (evt) => {
+console.log("Handle Second Event Listner");
+console.log(evt.type); // click
+});
+
+// handler 3
+
+let handler3 = (evt) => {
+console.log("Handle third Event Listner");
+}
+btn2.addEventListener("click",handler3);
+btn2.removeEventListener("click", handler3);
